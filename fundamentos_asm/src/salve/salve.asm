f687b8de566e0678d00dc5377bd0d35b47ed4691
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2024-03-16 10:06:32 -0300

    Ajustes no código salve, e ajuste no makefile

--- fundamentos_asm/src/salve/salve.asm
+++ fundamentos_asm/src/salve/salve.asm
@@ -1,5 +1,5 @@
-section .data
-    msg: 
+; section .data
+    msg:
         db "Salve Simpatia", 10      ; 'msg' - Rótulo dos dados definido
         db "teste de definicao", 10  ; 'db'  - Os dados são definidos como
                                      ; uma caideia de bytes
@@ -9,12 +9,13 @@ section .data
                                      ; 'msg' - Endereço do primeiro byte da mensagem
 section .bss
     name: resb 50
-    
+
 section .text
     global _start                ; A diretiva 'global' torna o rótulo '_start'
                                  ; visível de qualquer parte do programa
 
 _start:                          ; Aqui está o início do programa
+    mov byte[name], 0x45
     mov rax, 0x01                ; Chamada de systema 'sys_write'
     mov rdi, 0x01                ; Descritor de arquivos 1 (stdout)
     mov rsi, msg                 ; Ponteiro para string em memória
@@ -24,5 +25,5 @@ _start:                          ; Aqui está o início do programa
 
 _end:
     mov rax, 60
-    mov rdi, len
+    mov rdi, 0x00
     syscall
