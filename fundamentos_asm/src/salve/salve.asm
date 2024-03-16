4b4665ae9b5014f868616fcb7855b0f073965ac7
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2024-03-16 08:28:45 -0300

    Acréscimo de código
    Inclusão de reserva de bytes
    
    Inclusão de mais uma string
    
    Ajuste dos valores passados para os registradores de dec para hex

--- fundamentos_asm/src/salve/salve.asm
+++ fundamentos_asm/src/salve/salve.asm
@@ -1,19 +1,22 @@
 section .data
-    msg: db "Salve Simpatia", 0 ; 'msg' - Rótulo dos dados definido
-                                 ; 'db'  - Os dados são definidos como
-                                 ; uma caideia de bytes
-    len: equ $ - msg             ; 'len' - Rótulo do tamanho da mensagem
-                                 ; 'equ' - Pseudo-instrução para definir constantes
-                                 ; '$'   - Endereço do último byte escrito em memória
-                                 ; 'msg' - Endereço do primeiro byte da mensagem
-
+    msg: 
+        db "Salve Simpatia", 10      ; 'msg' - Rótulo dos dados definido
+        db "teste de definicao", 10  ; 'db'  - Os dados são definidos como
+                                     ; uma caideia de bytes
+    len: equ $ - msg                 ; 'len' - Rótulo do tamanho da mensagem
+                                     ; 'equ' - Pseudo-instrução para definir constantes
+                                     ; '$'   - Endereço do último byte escrito em memória
+                                     ; 'msg' - Endereço do primeiro byte da mensagem
+section .bss
+    name: resb 50
+    
 section .text
     global _start                ; A diretiva 'global' torna o rótulo '_start'
                                  ; visível de qualquer parte do programa
 
 _start:                          ; Aqui está o início do programa
-    mov rax, 1                   ; Chamada de systema 'sys_write'
-    mov rdi, 1                   ; Descritor de arquivos 1 (stdout)
+    mov rax, 0x01                ; Chamada de systema 'sys_write'
+    mov rdi, 0x01                ; Descritor de arquivos 1 (stdout)
     mov rsi, msg                 ; Ponteiro para string em memória
     mov rdx, len                 ; Constante com o tamanho da string
     syscall                      ; Invoca a chamada de sistema com os
@@ -21,5 +24,5 @@ _start:                          ; Aqui está o início do programa
 
 _end:
     mov rax, 60
-    mov rdi, 0
+    mov rdi, len
     syscall
