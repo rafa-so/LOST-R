43066e10354f5cb6ac7c12f80241515a8f20d5b1
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2024-03-21 09:04:59 -0300

    Criação de sub-rotinas encapsulando códigos repetitivos

--- fundamentos_asm/src/fale/fale.asm
+++ fundamentos_asm/src/fale/fale.asm
@@ -19,46 +19,27 @@ section .text
 
 _start:
 
-    ; Impessão da mensagem 01
-    mov rax, 1
-    mov rdi, 1
     mov rsi, msg1
     mov rdx, len1
-    syscall
+    call _print_str
 
-    ; Inclusão de dados do teclado
     mov rax, 0
     mov rdi, 0
     mov rsi, name
     mov rdx, 50
     syscall
 
-    ; Apresenta a segunda mensagem
-    mov rax, 1
-    mov rdi, 1
     mov rsi, msg2
     mov rdx, len2
-    syscall
+    call _print_str
 
     mov r8, name
 
-    _print:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, r8
-        mov rdx, 1
-        syscall
+    call _print
 
-        inc r8
-        cmp byte[r8], 10
-        jne _print 
-
-    ; Apresenta com a mensagem final
-    mov rax, 1
-    mov rdi, 1
     mov rsi, msg3
     mov rdx, len3
-    syscall
+    call _print_str
 
 _end:
 
@@ -66,3 +47,22 @@ _end:
     mov rdi, 0
     syscall
 
+_print_str:
+
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+_print:
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, r8
+    mov rdx, 1
+    syscall
+
+    inc r8
+    cmp byte[r8], 10
+    jne _print
+    ret
